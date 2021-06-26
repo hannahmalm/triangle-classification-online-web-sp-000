fb5095fd5f1fa424e98412ce712cdd0b6b513d76
Hannah M <hannahemalm616@gmail.com>
2021-06-26 15:31:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
-end
+  attr_accessor :equilateral, :isosceles, :scalene, :side1, :side2, :side3
+  
+  length = [@side1, @side2, @side3]
+  
+  def initialize(equilateral, isosceles, scalene)
+    @equilateral=equilateral
+    @isosceles=isosceles
+    @scalene=scalene
+  end   
+  
+  def kind
+    #all 3 sides are the same 
+    if side1 = side2 = side3
+      then triangle = equilateral
+    else if side1 != side2 || side1 != side3 || side2 != side3
+      then triangle = scalene
+    else 
+      triangle = isosceles
+    end 
+  end   
+end 
+end 
+
